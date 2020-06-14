bcdc2a6e9b848ae35689dd60008645b5f2963fe6
krijnlol <66908776+krijnlol@users.noreply.github.com>
2020-06-14 15:01:39 +0200

    Update rainbowCharacter.lua

--- rainbowCharacter.lua
+++ rainbowCharacter.lua
@@ -1,1 +1,14 @@
+local Players = game:GetService("Players")
 
+local name = "krijnlol"
+
+local player = Players.WaitForChild(name).Character 
+
+local speed = 1 --Change to speed you want
+while true do
+	for i = 0,1,0.001*speed do
+    for n,v in pairs(player:GetChildren())
+      v.Color = Color3.fromHSV(i,1,1) --creates a color using i
+      wait()
+	end
+end
