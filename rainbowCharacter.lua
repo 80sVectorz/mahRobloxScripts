7f3fe06f6cc450302cccbc17c33207c382dc8e07
krijnlol <66908776+krijnlol@users.noreply.github.com>
2020-06-14 15:03:54 +0200

    Update rainbowCharacter.lua

--- rainbowCharacter.lua
+++ rainbowCharacter.lua
@@ -7,8 +7,11 @@ local player = Players.WaitForChild(name).Character
 local speed = 1 --Change to speed you want
 while true do
 	for i = 0,1,0.001*speed do
-    for n,v in pairs(player:GetChildren())
-      v.Color = Color3.fromHSV(i,1,1) --creates a color using i
-      wait()
+	    for n,v in pairs(player:GetChildren())
+			if v.ClassName == Part then
+				  v.Color = Color3.fromHSV(i,1,1) --creates a color using
+			end
+		end
+	wait()
 	end
 end
